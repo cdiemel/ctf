8554def7751888a03d4d9ff33a0d3fbfd4d40256
Casey Diemel <diemelcw@gmail.com>
2019-11-12 12:46:41 -0500

    added argparse
    Signed-off-by: Casey Diemel <diemelcw@gmail.com>

--- scanning/hidden_UID.py
+++ scanning/hidden_UID.py
@@ -11,8 +11,25 @@ import time
 import socket
 import sys
 import math
+import argparse
 
-logging.basicConfig(filename='9090_UID.log', filemode='w', level=logging.DEBUG)
+warn = ""
+warn = warn+"** WARNING **\n"
+warn = warn+"THIS SENDS A SIGNIFICANT AMOUNT OF TRAFFIC\n"
+warn = warn+"DO NOT USE IF YOU DO NOT UNDERSTAND WHAT\n"
+warn = warn+"YOU ARE DOING, YOU HAVE BEEN WARNED."
+
+parser = argparse.ArgumentParser(prog=__file__,
+                                 description="Fuzz UID.\n"+warn,
+                                 epilog="UID fuzzer by C. Diemel\n")
+parser.add_argument('-a', metavar='', help='IP address', type=str, required=True)
+parser.add_argument('-p', metavar='', help='Port number', type=int, required=True)
+args = parser.parse_args()
+
+print(vars(args))
+
+fn = args.a.replace('.','-')+'_'+str(args.p)+'_UID.log'
+logging.basicConfig(filename=fn, filemode='w', level=logging.DEBUG)
 
 alpha   = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 num     = ['0','1','2','3','4','5','6','7','8','9']
@@ -54,7 +71,8 @@ try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         # Connect the socket to the port where the server is listening
-        server_address = ('52.21.1.196', 9090)
+        # server_address = ('52.21.1.196', 9090)
+        server_address = (args.a, args.p)
         print('connecting to %s port %s' % server_address)
         logging.info('connecting to %s port %s' % server_address)
         sock.connect(server_address)
