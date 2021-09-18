61b00bfffb39af23a5a84b6499e68d24e731306c
Casey Diemel <diemelcw@gmail.com>
2021-09-18 13:08:13 -0400

    one file to rule them all
    Signed-off-by: Casey Diemel <diemelcw@gmail.com>

---
+++ ScratchingPost.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+__author__ = 'C. Diemel'
+__version__ = '1.0.0'
+__name__ = 'ScratchingPost.py'
+__license__ = 'GPL2'
+__description__ = 'Script to navigate CDL challenges'
+
+
+class Post:
+    
+    def __init__(self):
+        pass
+    
+   
+   
+   
+    
+    
+# Open Source
+# Cryptography
+# Password Cracking
+# Log Analysis
+# Network Traffic Analysis
+# Wireless Access Exploitation
+# Forensics
+# Scanning
+# Web Application Exploitation
+# Enumeration and Exploitation
